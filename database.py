7d1eef6e79b5fb9b5b6e211f97b62572b1fa399a
Sebastian Meragelman <arielmeragelman@gmail.com>
2022-11-10 19:23:50 -0300

    se realizan todas las pruebas en main

--- database.py
+++ database.py
@@ -1,7 +1,7 @@
 import sqlalchemy as db
-from sqlalchemy import MetaData,Table,Column
+from sqlalchemy import MetaData, Table, Column
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import Column,String,Integer
+from sqlalchemy import Column, String, Integer
 from sqlalchemy.orm import sessionmaker
 
 Base = declarative_base()
@@ -42,77 +42,113 @@ class Database():
             print(data)
 
     def saveData(self, customer: list) -> None:
+        """saveData Guarda un registro en la tabla customers de la base de
+           datos
+           :param customer: lista con los datos a insertar en la tabla
+           :type customer: list
+        """
+        # Ejecutamos el insert de los valores pasados
         self.connection.execute(
             f"""INSERT INTO customer(name,age,email,address,zip_code) VALUES( '
             {customer.name}','{customer.age}','{customer.email}','
             {customer.address}','{customer.zip_code}')""")
-
         self.session.add(customer)
         self.session.commit()
 
-    def updateCustomer(self,customerName,address):
-        
-        dataToUpdate= {Customer.address: address}
-        customerData=self.session.query(Customer).filter(Customer.name==customerName)
-        customerData=update(dataToUpdate)
+    def updateCustomer(self, customerName: str,address: str) -> None:
+        """updateCustomer Actualizo la direccion de un registro en base del
+           nombre
+
+        :param customerName: nombre del cliente
+        :type customerName: str
+        :param address: Nueva direccion del cliente
+        :type address: str
+        """
+        dataToUpdate = {Customer.address: address}
+        customerData = self.session.query(Customer).filter(
+                       Customer.name == customerName)
+        #Funcion de update del modulo de sqlalchemy
+        customerData = update(dataToUpdate)
         self.session.commit()
 
-    def deleteCustomer(self,customer):
-        
-        customerData= self.session.query(Customer).filter(Customer.name==customer).first()
+    def deleteCustomer(self, customer: str) -> None:
+        """deleteCustomer Elimina registro del cliente en base al nombre
+
+        :param customer: Nombre del Cliente
+        :type customer: str
+        """
+
+        customerData = self.session.query(Customer).filter(
+                       Customer.name == customer).first()
         self.session.delete(customerData)
         self.session.commit()
 
-
-
-    def fetchUserByName(self):
+    def fetchUserByName(self) -> None:
+        """fetchUserByName Imprime la lista nombre de
+                           usuarios
+        """
         meta = MetaData()
         customer = Table('customer', meta, Column('name'))
         data = self.connection.execute(customer.select())
         for cust in data:
             print(cust)
 
-
     def fetchAllUsers(self):
-        self.session = Session(bind=self.connection)
+        """fetchAllUsers Imprime la lista de nombres de todos los usuarios
+        """
         customers = self.session.query(Customer).all()
         for cust in customers:
             print(cust)
 
 
 class Customer(Base):
+    """Customer Clase Customer, define la tabla customer y sus caracteristicas
 
+    :param Base: Clase padre que hereda a Customer las propiedades para poder usar el ORM
+    :type Base: objeto Base
+    
+    """
     __tablename__ = 'customer'
-    name= Column(String,primary_key=True)
-    age= Column(Integer)
-    email= Column(String)
-    address= Column(String)
-    zip_code= Column(String)
+    name = Column(String, primary_key=True)
+    age = Column(Integer)
+    email = Column(String)
+    address = Column(String)
+    zip_code = Column(String)
 
-
-    def __init__(self,name,age,email,address,zip_code):
+    def __init__(self, name, age, email, address, zip_code):
         self.name = name
         self.age = age
         self.email = email
         self.address = address
         self.zip_code = zip_code
 
-    def __repr__(self):
+    def __repr__(self) -> None:
+        """__repr__ Metodo para devolver un registro completo de la tabla
+
+        :return: Devuelve un registro de un cliente
+        :rtype: Objeto Customer
+        """
         return "<Customer(name= '%s', age= '%s',email= '%s',address= '%s',zip_code= '%s')>" % (self.name,self.age,self.email,self.address,self.zip_code)
 
 
-
-
-
 def main():
+    """main Funcion para ejecutar la secuencia completa de prueba
+    """
+
     db = Database()
-    address= Column(String)
     customer = Customer("Leonela", 28, "leo@gmail.com", "Av. Primera", "C1455")
+    customer2 = Customer("Juan Perez", 18, "marcelino@gmail.com",
+                         "Av. Segunda", "CZC455")
     db.saveData(customer)
+    db.saveData(customer2)
     db.fetchByQuery('public.customer')
-
-
+    db.fetchUserByName()
+    db.fetchAllUsers()
+    db.deleteCustomer("Leonela")
+    db.fetchUserByName()
+    # La funcion updateCustomer aun no funciona correctamente.
+    #  Pendiente de corregir
+    #db.updateCustomer("Juan Perez","Av.Tercera")
 
 if __name__ == "__main__":
     main()
-
